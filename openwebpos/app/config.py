1e5957f613a9590f21aed93b01a7cff84330f152
Javier Baez <baezdevs@gmail.com>
2023-11-13 12:45:00 -0600

    Updated config file for environment variables in openwebpos application
    Significantly extended the configuration file to include Flask settings, application settings, and database settings. The update uses environment variables for better security and configuration flexibility. The progress of the application configuration and startup can now be inferred from environment variables which enhances debugging ability. Additionally, the previous binary variable "DB_CONFIGURED" has been made more flexible with additional steps of the configuration process being reflected.

--- openwebpos/app/config.py
+++ openwebpos/app/config.py
@@ -1,7 +1,30 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# Flask settings
+DEBUG = os.getenv("DEBUG", False)
+SECRET_KEY = os.getenv("SECRET_KEY", "test-secret-key")
 
-DB_CONFIGURED = os.getenv("DB_CONFIGURED", "False") == "True"
+# Application settings
+TIMEZONE = os.getenv("TIMEZONE", "US/Central")
+APP_PATH = os.getenv(
+    "APP_PATH", os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+)
+APP_DIR = os.getenv(
+    "APP_DIR",
+    os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),
+)
+DB_CONFIGURED = os.getenv("DB_CONFIGURED", False)
+APP_RESTARTED = os.getenv("APP_RESTARTED", False)
+USER_CONFIGURED = os.getenv("USER_CONFIGURED", False)
+APP_CONFIGURED = os.getenv("APP_CONFIGURED", False)
+SITE_NAME = os.getenv("SITE_NAME", "Open Web POS")
+
+# Database settings
+if DB_CONFIGURED:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
