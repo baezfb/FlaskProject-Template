09eb7ff73113a4698571270404325fd7b488dc8f
Javier Baez <baezdevs@gmail.com>
2023-11-13 04:40:47 -0600

    Add 'click-shell' and interactive shell for blueprint management
    This commit introduces 'click-shell' as a new development requirement, and establishes an interactive shell environment with the 'addblueprint' command. This command enables users to add blueprints to the app. The code checks if the blueprint already exists, if not, it creates the related folders and __init__.py files for models, views, and forms.
    
    The aim of this change is to streamline the creation of blueprints in the "openwebpos" application. This eliminates the manual process of creating directories and files, providing an efficient and error-proof way to manage blueprints.

---
+++ cli.py
@@ -0,0 +1,55 @@
+import os
+
+from click_shell import shell
+
+blueprint_path = os.path.join("openwebpos", "blueprints")
+print(blueprint_path)
+
+
+@shell(prompt="openwebpos-shell > ", intro="Starting openwebpos shell...")
+def openwebpos():
+    pass
+
+
+@openwebpos.command()
+def addblueprint():
+    """Add a blueprint to the app"""
+    blueprint_name = input("Enter the name of the blueprint: ")
+    # Check if blueprint already exists
+    if os.path.exists(os.path.join(blueprint_path, blueprint_name)):
+        print("Blueprint already exists")
+        return
+    # Create blueprint folder
+    os.makedirs(os.path.join(blueprint_path, blueprint_name))
+    # Create __init__.py file
+    with open(os.path.join(blueprint_path, blueprint_name, "__init__.py"), "w") as f:
+        f.write(
+            f"from flask import Blueprint\n\n{blueprint_name}_bp = Blueprint('{blueprint_name}', __name__, template_folder='templates')\n"
+        )
+    # Add models folder to blueprint
+    os.makedirs(os.path.join(blueprint_path, blueprint_name, "models"))
+    # Create __init__.py file
+    with open(
+        os.path.join(blueprint_path, blueprint_name, "models", "__init__.py"), "w"
+    ) as f:
+        f.write("")
+    # Add views folder to blueprint
+    os.makedirs(os.path.join(blueprint_path, blueprint_name, "views"))
+    # Create __init__.py file
+    with open(
+        os.path.join(blueprint_path, blueprint_name, "views", "__init__.py"), "w"
+    ) as f:
+        f.write("")
+    # Add forms folder to blueprint
+    os.makedirs(os.path.join(blueprint_path, blueprint_name, "forms"))
+    # Create __init__.py file
+    with open(
+        os.path.join(blueprint_path, blueprint_name, "forms", "__init__.py"), "w"
+    ) as f:
+        f.write("")
+    with open("openwebpos/blueprints/__init__.py", "a") as f:
+        f.write(f"from .{blueprint_name} import {blueprint_name}_bp\n")
+
+
+if __name__ == "__main__":
+    openwebpos()
