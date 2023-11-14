945b7222058bd41360d1337df51709bdb226064c
Javier Baez <baezdevs@gmail.com>
2023-11-14 11:38:40 -0600

    Correct application config settings in openwebpos
    Updated the `config.py` in the openwebpos application to replace environment variable settings with static values. Removed the use of environment variables for debug, secret key, timezone, app path, etc. and replaced them with hard-coded values for consistency and to reduce dependency on the environment. Converted database configuration from dynamic to static; SQLite is now used in a consistent way. This may limit the flexibility of the application, but it increases predictability and strengthens secure handling of data.

--- openwebpos/app/config.py
+++ openwebpos/app/config.py
@@ -5,26 +5,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Flask settings
-DEBUG = os.getenv("DEBUG", False)
-SECRET_KEY = os.getenv("SECRET_KEY", "test-secret-key")
+SECRET_KEY = (
+    "TestSecret-26b8cc7c6f7afabf828bf647c898244034c708e0d8513a04976bd561c67d9714"
+)
 
 # Application settings
-TIMEZONE = os.getenv("TIMEZONE", "US/Central")
-APP_PATH = os.getenv(
-    "APP_PATH", os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-)
-APP_DIR = os.getenv(
-    "APP_DIR",
-    os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),
-)
-DB_CONFIGURED = os.getenv("DB_CONFIGURED", False)
-APP_RESTARTED = os.getenv("APP_RESTARTED", False)
-USER_CONFIGURED = os.getenv("USER_CONFIGURED", False)
-APP_CONFIGURED = os.getenv("APP_CONFIGURED", False)
-SITE_NAME = os.getenv("SITE_NAME", "Open Web POS")
+TIMEZONE = "US/Central"
+APP_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+APP_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+UUID_NAMESPACE = "www.openwebpos.com"
 
 # Database settings
-if DB_CONFIGURED:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = False
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{APP_DIR}/openwebpos.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = False
+
+TEST = "Test from default config file"
