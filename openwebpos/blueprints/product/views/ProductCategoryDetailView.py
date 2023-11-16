e6f053e06a8ba6d1931fc8f3a27f3304d3e9e4fe
Javier Baez <baezdevs@gmail.com>
2023-11-16 00:35:16 -0600

    In the 'ProductCategoryDetailView', added PUT and DELETE methods to enhance functionality and interactivity. The specific id is now used to update and delete a specific product. A new feature was also added to validate form submission and instantaneously convert inputted price to cents to ensure consistency in data format across the system.

--- openwebpos/blueprints/product/views/ProductCategoryDetailView.py
+++ openwebpos/blueprints/product/views/ProductCategoryDetailView.py
@@ -1,7 +1,8 @@
-from flask import url_for
+from flask import url_for, request
 
 from openwebpos.app.views import DetailView
 from openwebpos.utils.htmx import htmx_refresh
+from openwebpos.utils.money import convert_to_cents
 from ..forms.ProductForm import ProductForm
 from ..models import ProductCategory, Product
 
@@ -30,8 +31,18 @@ class ProductCategoryDetailView(DetailView):
             )
         return self.redirect(url_for("product.category", id=id))
 
-    def put(self):
+    def put(self, id):
+        form = self.form()
+        if form.validate_on_submit():
+            self.product_model.get_by_id(request.form.get("id")).update(
+                name=form.name.data,
+                short_name=form.short_name.data,
+                description=form.description.data,
+                price=convert_to_cents(float(form.price.data)),
+                active=form.active.data,
+            )
         return htmx_refresh()
 
-    def delete(self):
+    def delete(self, id):
+        self.product_model.get_by_id(request.form.get("id")).delete()
         return htmx_refresh()
