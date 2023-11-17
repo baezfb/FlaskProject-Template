8df07e196fbe49c3d55815e6cdc1357c14612ab8
Javier Baez <baezdevs@gmail.com>
2023-11-17 01:34:20 -0600

    Revise ProductIngredientForm for detailed ingredient tracking
    The changes made to ProductIngredientForm are aimed at improving ingredient tracking. Previously, the form only required 'ingredient_id' and 'price.' Now it additionally requires the 'amount' and 'unit' fields to provide more precise tracking. An assumed active ingredient is no longer hardcoded and is now extracted dynamically from active_not associated ones. It broadens the ingredients' applicable scope and ensures its flexibility on different scenarios. Also, options of unit values have been added to enable a variety of measurements.

--- openwebpos/blueprints/product/forms/ProductIngredientForm.py
+++ openwebpos/blueprints/product/forms/ProductIngredientForm.py
@@ -1,18 +1,37 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, InputRequired
 
-from openwebpos.utils.wtforms import CustomSubmitField, MoneyField
-from ..models import Ingredient
+from openwebpos.utils.wtforms import CustomStringField, CustomSubmitField, MoneyField
+from ..models import Ingredient, ProductIngredient
 
 
 class ProductIngredientForm(FlaskForm):
     ingredient_id = SelectField("Ingredient", coerce=str, validators=[DataRequired()])
-    price = MoneyField("Price")
+    amount = CustomStringField("Amount", validators=[DataRequired()])
+    unit = SelectField("Unit", coerce=str, validators=[DataRequired()])
     submit = CustomSubmitField("Submit")
 
     def __init__(self, *args, **kwargs):
         super(ProductIngredientForm, self).__init__(*args, **kwargs)
-        self.ingredient_id.choices = [
-            (i.id, i.name) for i in Ingredient.query.filter_by(active=True).all()
+        associated_ingredients = [
+            p.ingredient_id
+            for p in ProductIngredient.query.filter_by(
+                product_id=kwargs.get("product_id")
+            )
+        ]
+        active_ingredients = Ingredient.query.filter_by(active=True).all()
+        active_not_associated = [
+            i for i in active_ingredients if i.id not in associated_ingredients
+        ]
+        self.ingredient_id.choices = [(c.id, c.name) for c in active_not_associated]
+        # self.ingredient_id.choices = [
+        #     (i.id, i.name) for i in Ingredient.query.filter_by(active=True).all()
+        # ]
+        self.unit.choices = [
+            ("oz", "oz"),
+            ("lb", "lb"),
+            ("g", "g"),
+            ("ml", "ml"),
+            ("each", "each"),
         ]
