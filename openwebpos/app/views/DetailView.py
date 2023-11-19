fab30428fe3c437765fb1e5cefc7fdb967a6c96d
Javier Baez <baezdevs@gmail.com>
2023-11-19 02:17:33 -0600

    Add flexibility to queries in DetailView.py
    Modified the DetailView.py to provide flexibility in the kind of queries conducted. Added two new attributes `url_variable` and `query_field` which allow specifying the field to query and the reference from the URL. Also updated `get_default_context` method to set navigation and page title based on the queried object's name. This change enhances the modularity and increases the utility of DetailView.

--- openwebpos/app/views/DetailView.py
+++ openwebpos/app/views/DetailView.py
@@ -9,6 +9,8 @@ class DetailView(TemplateView):
     init_every_request = False
 
     model = None
+    url_variable = "var"  # Used to get the object from the URL
+    query_field = None  # field to query. If none it will query the id field and use url_variable as the id
     context_object_name = "object"
 
     def get_context_object_name(self):
@@ -25,12 +27,38 @@ class DetailView(TemplateView):
         if self.model is None:
             error = "%s must define `get_object()`"
             raise NotImplementedError(error % self.__class__.__name__)
-        return self.model.query.get_or_404(self.kwargs["id"])
+
+        if self.query_field:
+            return self.model.query.filter_by(
+                **{self.query_field: self.kwargs[self.url_variable]}
+            ).first_or_404()
+        else:
+            return self.model.query.get_or_404(self.kwargs[self.url_variable])
+
+        # if self.request_variable == "id":
+        #     return self.model.query.get_or_404(self.kwargs[self.request_variable])
+        # elif self.request_variable == "slug":
+        #     return self.model.query.filter_by(
+        #         slug=self.kwargs[self.request_variable]
+        #     ).first_or_404()
+        # else:
+        #     raise NotImplementedError("request_variable must be either 'id' or 'slug'")
+        # return self.model.query.get_or_404(self.kwargs[self.request_variable])
 
     def get_default_context(self):
         """
         Add the object to the context.
         """
+        if self.nav_title is None:
+            if self.get_object().name:
+                self.nav_title = self.get_object().name.title()
+
+        if self.title is None:
+            if self.get_object().name:
+                self.title = self.get_object().name.title() + " Details"
+            else:
+                self.title = "Details"
+
         context = super().get_default_context()
         context[self.get_context_object_name()] = self.get_object()
         return context
