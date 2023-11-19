4d9051cebd32182701b2d323c698728cc49c041c
Javier Baez <baezdevs@gmail.com>
2023-11-19 13:00:25 -0600

    Add HiddenField to CategoryTypeForm and category_detail template
    Introduced a HiddenField 'category_id' to the CategoryTypeForm in OpenWebPos to hold each category's id when editing. Updated the category_detail template to accommodate the new field in both edit and add modals. This was necessary to ensure accurate and efficient fetch and update of category details.

--- openwebpos/blueprints/ingredient/forms/CategoryTypeForm.py
+++ openwebpos/blueprints/ingredient/forms/CategoryTypeForm.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField
+from wtforms import BooleanField, HiddenField
 from wtforms.validators import DataRequired, Length
 
 from openwebpos.utils.wtforms import CustomStringField, CustomSubmitField
 
 
 class CategoryTypeForm(FlaskForm):
+    category_id = HiddenField("Category ID")
     name = CustomStringField("Name", validators=[DataRequired(), Length(min=4, max=24)])
     active = BooleanField("Active")
     submit = CustomSubmitField("Submit")
